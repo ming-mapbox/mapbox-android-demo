d502f79c0c7f32fab15798361494b215607723ee
langsmith <langstonlmcs@gmail.com>
2018-10-20 09:52:24 +0800

    review tweaks

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -63,7 +63,7 @@ import com.mapbox.mapboxandroiddemo.examples.extrusions.PopulationDensityExtrusi
 import com.mapbox.mapboxandroiddemo.examples.extrusions.RotationExtrusionActivity;
 import com.mapbox.mapboxandroiddemo.examples.javaservices.DirectionsActivity;
 import com.mapbox.mapboxandroiddemo.examples.javaservices.GeocodingActivity;
-import com.mapbox.mapboxandroiddemo.examples.javaservices.IsochoneActivity;
+import com.mapbox.mapboxandroiddemo.examples.javaservices.IsochroneActivity;
 import com.mapbox.mapboxandroiddemo.examples.javaservices.MapMatchingActivity;
 import com.mapbox.mapboxandroiddemo.examples.javaservices.MatrixApiActivity;
 import com.mapbox.mapboxandroiddemo.examples.javaservices.OptimizationActivity;
@@ -863,7 +863,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
       R.id.nav_java_services,
       R.string.activity_java_services_isochrone_title,
       R.string.activity_java_services_isochrone_description,
-      new Intent(MainActivity.this, IsochoneActivity.class),
+      new Intent(MainActivity.this, IsochroneActivity.class),
       null,
       R.string.activity_java_services_isochrone_url, false, BuildConfig.MIN_SDK_VERSION));
 
